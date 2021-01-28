b5f96520133d58a97652572814cbb178bf524590
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-01-28 16:00:42 +0100

    match paramaters from org. GSA paper

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,15 @@ mod gsa_paper {
 }
 
 fn main() {
-    let g0 = r32(100f32);
-    let alpha = r32(20f32);
-    let t0 = r32(20f32);
-    let max_n = 5;
-    const SEED: u64 = 0;
-    const POPULATION: usize = 2;
-    const DIMENSION: usize = 2;
+    let g0 = r32(100f32); // check
+    let alpha = r32(20f32); // check
+    let t0 = r32(1_000f32); // unused
+    let max_n = 1_000; // check
+    const SEED: u64 = 1;
+    const POPULATION: usize = 50; // check
+    const DIMENSION: usize = 30; // check
 
     let stop = |n: usize, _| n > max_n;
     let mut gsa: GSA<R32, _, Minimize, _, DIMENSION> = GSA::new(g0, t0, alpha, max_n, SEED, gsa_paper::f1, stop);
-    gsa.search(r32(-5f32)..=r32(5f32), POPULATION);
+    gsa.search(r32(-100f32)..=r32(100f32), POPULATION);
 }
